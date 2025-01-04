2878b3018f1d8243c4cbb31c0abc31e41ea324df
Rajesh97266 <rajesh97266@gmail.com>
2025-01-04 19:47:45 +0530

    images-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import carImage from "./assets/car_image.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="/" className="logo">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="imageBox">
+          <img src={carImage} alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Rajesh</a>
+            <time>2024-01-05 19:40</time>
+          </p>
+          <p className="description">
+            Founded in 1937, Volkswagen is a German automaker that has gained a
+            reputation for building some of the most iconic cars in the world
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="imageBox">
+          <img src={carImage} alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Rajesh</a>
+            <time>2024-01-05 19:40</time>
+          </p>
+          <p className="description">
+            Founded in 1937, Volkswagen is a German automaker that has gained a
+            reputation for building some of the most iconic cars in the world
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="imageBox">
+          <img src={carImage} alt="" />
+        </div>
+        <div className="text">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Rajesh</a>
+            <time>2024-01-05 19:40</time>
+          </p>
+          <p className="description">
+            Founded in 1937, Volkswagen is a German automaker that has gained a
+            reputation for building some of the most iconic cars in the world
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
